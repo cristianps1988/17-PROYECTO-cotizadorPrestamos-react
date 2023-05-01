cf66d1584b476c69365917383ff070c6668adefc
Cristianps1988 <cristianps_1@hotmail.com>
2023-05-01 14:17:40 -0500

    project finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import Header from './components/Header'
+import Button from './components/Button'
+import { formatearDinero, calcularTotalPagar } from './helpers'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cantidad, setCantidad] = useState(10000)
+  const [meses, setMeses] = useState(6)
+  const [total, setTotal] = useState(0)
+  const [pago, setPago] = useState(0)
+
+  useEffect(() => {
+    const resultadoTotalPagar = calcularTotalPagar(cantidad, meses)
+    setTotal(resultadoTotalPagar)
+  }, [cantidad, meses])
+
+  useEffect(() => {
+    setPago(total / meses)
+  }, [total])
+
+  const MIN = 0
+  const MAX = 20000
+  const STEP = 100
+
+  function handleChange(e) {
+    setCantidad(parseInt(e.target.value))
+  }
+
+  function handlerClickMenos() {
+    const valor = cantidad - STEP
+    if (cantidad <= MIN) {
+      alert('Cantidad no válida')
+      return
+    }
+    setCantidad(valor)
+  }
+
+  function handlerClickMas() {
+    const valor = cantidad + STEP
+    if (cantidad >= MAX) {
+      alert('Cantidad no válida')
+      return
+    }
+    setCantidad(valor)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10 rounded-lg ">
+      <Header />
+      <div className="flex justify-between my-6">
+        <Button
+          operador='-'
+          fn={handlerClickMenos}
+        />
+        <Button
+          operador='+'
+          fn={handlerClickMas}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <input
+        type="range"
+        className='w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600'
+        onChange={handleChange}
+        min={MIN}
+        max={MAX}
+        step={STEP}
+        value={cantidad}
+      />
+      <p className='text-center text-5xl font-extrabold my-10 text-indigo-600' >{formatearDinero(cantidad)}</p>
+
+      <h2 className="text-2xl font-extrabold text-gray-500 text-center">Elige un <span className='text-indigo-600'>plazo</span> a pagar:</h2>
+
+      <select
+        className="mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500"
+        value={meses}
+        onChange={e => setMeses(parseInt(e.target.value))}
+      >
+        <option value="6">6 Meses</option>
+        <option value="12">12 Meses</option>
+        <option value="24">24 Meses</option>
+      </select>
+
+      <div className="my-5 space-y-3 bg-gray-50-p-5">
+        <h2 className="text-2xl font-extrabold text-gray-500 text-center">Resumen de <span className='text-indigo-600'>pagos</span></h2>
+        <p className="text-xl text-gray-500 text-center font-bold">{meses} meses</p>
+        <p className="text-xl text-gray-500 text-center font-bold"> {formatearDinero(total)} Total a Pagar</p>
+        <p className="text-xl text-gray-500 text-center font-bold"> {formatearDinero(pago)} Mensuales</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
   )
 }
 
